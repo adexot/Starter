e39c5a32b103a8c487ab3c0f7b78164fed8e1648
Adeyemi Malik <adeyemimalik@hotmail.com>
2017-11-01 18:07:19 +0100

    recycler view added

--- app/src/main/java/com/oadex/starter/FragmentThree.java
+++ app/src/main/java/com/oadex/starter/FragmentThree.java
@@ -3,63 +3,57 @@ package com.oadex.starter;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentThree#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FragmentThree extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import com.oadex.starter.model.DataBaseHelper;
+import com.oadex.starter.model.Starter;
+import com.oadex.starter.model.StarterAdapter;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+import java.util.List;
 
+public class FragmentThree extends Fragment
+{
+    private RecyclerView recyclerView;
+    private List<Starter> list;
+    private StarterAdapter adapter;
+    private DataBaseHelper database;
 
-    public FragmentThree() {
+    public FragmentThree()
+    {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment FragmentThree.
-     */
-    // TODO: Rename and change types and number of parameters
     public static FragmentThree newInstance() {
         FragmentThree fragment = new FragmentThree();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        database = new DataBaseHelper(getContext());
+        list = database.getAll();
+        adapter = new StarterAdapter(list);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-//        TextView textView = new TextView(getActivity());
-//        textView.setText(R.string.hello_blank_fragment);
-//        return textView;
-        return inflater.inflate(R.layout.fragment_three, container, false);
+
+        View rootView =  inflater.inflate(R.layout.fragment_three, container, false);
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layout);
+
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
